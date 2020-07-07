30b7cda2a628276e475c3ef177e363a860edecc0
Kerollos Magdy <kerolloz@yahoo.com>
2020-07-07 14:15:59 +0200

    use darkmode & time counter when game starts

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="board-container">
     <h1 class="title">8-Puzzle Game</h1>
-    <table class="board-table">
+    <table class="board-table" :class="{ solved: isSolved }">
       <tbody>
         <Row
           v-for="(row, index) in board"
@@ -12,10 +12,11 @@
         />
       </tbody>
     </table>
-    <span class="game-state">
-      {{ totalMoves }} Move{{ totalMoves !== 1 ? "s" : "" }}
-    </span>
-    <span class="game-state" v-if="isSolved">Great Job ✅</span>
+    <div class="state-container game-state">
+      <span>{{ timer }} Second{{ timer !== 1 ? "s" : "" }} </span>
+      <span> {{ totalMoves }} Move{{ totalMoves !== 1 ? "s" : "" }} </span>
+    </div>
+    <span class="game-state green" v-if="isSolved">Great Job ✅</span>
   </div>
 </template>
 
@@ -35,7 +36,9 @@ export default {
   data() {
     return {
       board: getBoard(true),
+      gameStarts: false,
       totalMoves: 0,
+      timer: 0,
     };
   },
   created() {
@@ -43,7 +46,6 @@ export default {
   },
   computed: {
     isSolved() {
-      console.log("check");
       for (let i = 0; i < 3; i++)
         for (let j = 0; j < 3; j++)
           if (this.board[i][j] !== correctBoard[i][j]) return false;
@@ -51,9 +53,20 @@ export default {
       return true;
     },
   },
+  watch: {
+    gameStarts() {
+      this.timer++;
+    },
+    timer() {
+      setTimeout(() => {
+        if (!this.isSolved && this.totalMoves) this.timer++;
+      }, 1000);
+    },
+  },
   methods: {
     onTileClicked(clickedTile) {
-      if (isSamePoint(clickedTile, this.blank)) return; // do nothing
+      if (this.isSolved || isSamePoint(clickedTile, this.blank)) return; // do nothing
+      this.gameStarts = true;
       const moves = [
         { x: 1, y: 0 },
         { x: -1, y: 0 },
@@ -82,14 +95,20 @@ export default {
 
 <style scoped>
 .board-container {
+  background-color: #222;
   width: 100%;
   height: 100%;
   display: flex;
-  align-content: center;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  justify-items: center;
+}
+
+.state-container {
+  margin: 0 auto;
+  min-width: 300px;
+  justify-content: space-around;
+  display: flex;
 }
 
 .board-table {
@@ -98,10 +117,23 @@ export default {
 }
 
 .title {
-  color: #222;
+  color: #ddd;
+}
+
+.solved {
+  background-color: rgba(50, 255, 50, 0.2);
+}
+
+.green {
+  color: rgb(0, 230, 0);
+  text-shadow: 0.3px 0.3px #222;
 }
 
 .game-state {
+  border-radius: 5px;
+  background-color: #555;
+  color: #ddd;
+  padding: 10px;
   font-size: 20px;
   margin-top: 20px;
 }
